b295724f537430786301a02eb234cc7ddb5b7643
Gabriel Ayres <gbayres@hotmail.com>
2020-10-03 23:27:40 -0300

    Novo

---
+++ imc2 (novo).py
@@ -0,0 +1,40 @@
+categorias = { "m": { 10 : [14.42, 19.60], 
+                      11 : [14.83, 20.35],
+                      12 : [15.24, 21.12],
+                      13 : [15.74, 21.93],
+                      14 : [16.18, 22.77],
+                      15 : [16.59, 23.63],
+                      16 : [17.01, 24.45],
+                      17 : [17.31, 25.28],
+                      18 : [17.54, 25.95],
+                      19 : [17.80, 26.36],},
+
+               "f": { 10 : [14.23, 20.19],
+                      11 : [14.60, 21.18],
+                      12 : [14.98, 22.17],
+                      13 : [15.36, 23.08],
+                      14 : [15.67, 23.88],
+                      15 : [16.01, 24.29],
+                      16 : [16.37, 24.74],
+                      17 : [16.59, 25.23],
+                      18 : [16.71, 25.56],
+                      19 : [16.87, 25.85],},
+            }
+
+peso = float(input("Digite o seu peso em Kg: "))
+altura = float(input("Digite sua altura em m: "))
+idade = int(input("Digite sua idade: "))
+sexo = input("Digite o seu sexo (m ou f): ")
+
+imc = round(peso / (altura ** 2), 2)
+print("Seu IMC é: ", imc)
+
+
+if imc < categorias[sexo][idade][0]: 
+    print("Baixo Peso")
+
+elif imc < categorias[sexo][idade][1]:
+    print("Adequado")
+
+else:
+    print("Abaixo do peso")
